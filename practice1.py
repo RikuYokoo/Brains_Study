e82e2afcbcc0f7bc9724d7a90eef973b88e5eba6
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2020-11-21 02:09:26 +0900

    upload file

---
+++ practice1.py
@@ -0,0 +1,10 @@
+print("while")
+i = 0
+while i < 5:
+    print(i+1)
+    i += 1
+
+print("for")
+i = 0
+for i in range(5):
+    print(i+1)
---
+++ practice2.py
@@ -0,0 +1,24 @@
+a = 10
+b = 5
+c = 12
+
+if a < b and a < c:
+    mini = a
+    if b < c:
+        maax = c
+        mid = b
+    else:
+        maax = b
+        mid = c
+elif b < c:
+    mini = b
+    if a < c:
+        maax = c 
+        mid = a
+else:
+    mini = c
+    maax = b
+    mid = a
+
+print(mini, mid, maax)
+
